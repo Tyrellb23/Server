b2b00fb8d9d0b89fc3dc5b949f5ee7aba9c4cee8
Tyrellb23 <59323866+Tyrellb23@users.noreply.github.com>
2025-01-15 20:18:07 -0600

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask, request
+
 
 app = Flask(__name__)
 
@@ -24,4 +26,5 @@ def log_ip():
         return {"status": "error", "message": str(e)}, 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
