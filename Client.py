24ef14cececcb8b9934c769d1d981805e790c8b6
Tyrellb23 <59323866+Tyrellb23@users.noreply.github.com>
2025-01-17 08:15:06 -0600

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,16 +1,1 @@
-from flask import Flask, redirect, send_from_directory
-import os
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return redirect("https://www.google.com")
-
-# Serve favicon from static folder
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
-
-if __name__ == "__main__":
-    app.run()
