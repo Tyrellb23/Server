c264f509636a406815f4f1d5fb608f1dfc0d1bb1
Tyrellb23 <59323866+Tyrellb23@users.noreply.github.com>
2025-01-17 04:26:40 -0600

    Update Client.py

--- Client.py
+++ Client.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return redirect("https://www.google.com")
 
+# Ignore favicon.ico requests with a 204 status
+@app.route('/favicon.ico')
+def favicon():
+    return '', 204  # No content
+
 if __name__ == "__main__":
     app.run()
