0fbee505b294345e23ab0ca8fa05a0f6cd97cca0
Tyrellb23 <59323866+Tyrellb23@users.noreply.github.com>
2025-01-17 08:55:39 -0600

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,1 +1,11 @@
 
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return redirect("https://www.google.com")
+
+if __name__ == "__main__":
+    app.run()
